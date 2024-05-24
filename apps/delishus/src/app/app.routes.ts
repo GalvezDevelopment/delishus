0f65db26e196f2852c3ca48f3ac57998e56f9d31
Christian Galvez <christian.javalera@teamknowlogy.com>
2024-05-24 12:00:28 -0700

    DEL-1 Micfrofrontend architecture implemented with Nx

--- apps/delishus/src/app/app.routes.ts
+++ apps/delishus/src/app/app.routes.ts
@@ -1,3 +1,17 @@
+import { NxWelcomeComponent } from './nx-welcome.component';
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: 'waiters',
+    loadChildren: () => import('waiters/Routes').then((m) => m.remoteRoutes),
+  },
+  {
+    path: 'profile',
+    loadChildren: () => import('profile/Routes').then((m) => m.remoteRoutes),
+  },
+  {
+    path: '',
+    component: NxWelcomeComponent,
+  },
+];
