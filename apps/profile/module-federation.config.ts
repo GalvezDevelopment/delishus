00d02e4a4e06199e99c76480e1ed4aeaa367386c
Christian Galvez <christian.javalera@teamknowlogy.com>
2024-05-25 16:23:51 -0700

    .

--- apps/profile/module-federation.config.ts
+++ apps/profile/module-federation.config.ts
@@ -3,7 +3,7 @@ import { ModuleFederationConfig } from '@nx/webpack';
 const config: ModuleFederationConfig = {
   name: 'profile',
   exposes: {
-    './Routes': 'profile/src/app/remote-entry/entry.routes.ts',
+    './Routes': 'apps/profile/src/app/remote-entry/entry.routes.ts',
   },
 };
 
